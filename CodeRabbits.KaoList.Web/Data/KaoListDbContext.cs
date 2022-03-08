5ea58aa3a54a05c1a65f9b23fd92dd283016125d
Black <black@coderabbits.com>
2022-03-08 09:40:48 +0900

    초기 커밋.

---
+++ CodeRabbits.KaoList.Web/Data/KaoListDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeRabbits.KaoList.Web.Data
+{
+    public class KaoListDbContext : IdentityDbContext
+    {        
+        public KaoListDbContext(DbContextOptions<KaoListDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
