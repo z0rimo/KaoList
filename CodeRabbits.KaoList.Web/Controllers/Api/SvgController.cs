0d346deb9984440c0dbb05ffdac2324d21f74714
Black <black@coderabbits.com>
2022-04-04 11:58:43 +0900

    The svg file has been modified to be imported through the api.

---
+++ CodeRabbits.KaoList.Web/Controllers/Api/SvgController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CodeRabbits.KaoList.Web.Controllers.Api
+{
+    [Route("[controller]/{id}")]
+    [ApiController]
+    public class SvgController : ControllerBase
+    {
+        static HashSet<string> colorTagSet = new HashSet<string>()
+        {
+            "color",
+            "fill",
+            "stroke"
+        };
+
+        private string _svgPath { init; get; }
+        public SvgController(IConfiguration configuration)
+        {
+            _svgPath = configuration["SvgFilePath"];
+        }
+
+        public async Task<IActionResult> Index(string id)
+        {
+            var path = $@"{_svgPath}\{id}";
+            if (!System.IO.File.Exists(path))
+            {
+                return NotFound();
+            }
+
+            XDocument svg = XDocument.Load($@"{_svgPath}\{id}");
+            XElement? svgElement = svg.Root;
+            if (svgElement != null)
+            {
+                foreach (var query in Request.Query)
+                {
+                    if (colorTagSet.Contains(query.Key) && (query.Value.SingleOrDefault() is not null ? Regex.IsMatch(query.Value.Single(), "[a-f|A-F|0-9]{1,8}") : false))
+                    {
+                        svgElement.SetAttributeValue(query.Key, '#' + query.Value);
+                    }
+                    else
+                    {
+                        svgElement.SetAttributeValue(query.Key, query.Value);
+                    }
+                }
+            }
+
+            XmlWriterSettings settings = new()
+            {
+                Async = true
+            };
+
+            Response.StatusCode = 200;
+            Response.Headers.ContentType = "image/svg+xml";
+            Response.Headers.ETag = (CalculateMD5(path) ^ Request.QueryString.ToString().GetHashCode()).ToString("x4");
+            using XmlWriter writer = XmlWriter.Create(Response.BodyWriter.AsStream(), settings);
+            await svg.WriteToAsync(writer, CancellationToken.None);
+
+            return new EmptyResult();
+        }
+
+        static int CalculateMD5(string filename)
+        {
+            using var md5 = MD5.Create();
+            using var stream = System.IO.File.OpenRead(filename);
+            return BitConverter.ToInt32(md5.ComputeHash(stream));
+
+        }
+    }
+}
