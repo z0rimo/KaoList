9db7b1f694fef7959c95f2a6cdf8aa540b675343
Red <red@coderabbits.com>
2022-04-13 00:36:21 +0900

    Create login page (#50)
    * Create account layout style
    
    * Create login style
    
    * Delete space
    
    * Add tagehelper function
    
    * Modify login cshtml
    
    * Modify login cshtml (external logins, password input box)
    
    * Remove requirestwofactor conditional
    
    * Create login style
    
    * Modify viewstart path
    
    * Modify eye location
    
    * Remove inputbox focus outline
    
    * Add style conditional statement
    
    * Remove input box height
    
    * Create logout page
    
    * Modify logout button from asp to form
    
    * Add logout-button style
    
    * Apply disallow null values
    
    * Remove logout contents
    
    * Remove duplicate code
    
    Co-authored-by: Red <red@codrabbits.com>

---
+++ CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CodeRabbits.KaoList.Web.Areas.Identity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        public IActionResult OnGet()
+        {
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                // This needs to be a redirect so that the browser performs a new
+                // request and the identity for the user gets updated.
+                return LocalRedirect("/");
+            }
+        }
+    }
+}
