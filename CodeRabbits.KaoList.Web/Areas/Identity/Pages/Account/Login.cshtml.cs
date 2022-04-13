422e4ba1628362f583e7dbca8738fa154d53a07f
Red <red@coderabbits.com>
2022-04-14 00:15:47 +0900

    Bundle of required identity pages (#52)
    * Create account layout style
    
    * Create login style
    
    * Add tagehelper function
    
    * Update account layout style
    
    * Modify login cshtml
    
    * Remove requirestwofactor conditional
    
    * Create agreement page
    
    * Modify register
    
    * Required identity-page bundle reset
    
    * Remove agreement page
    
    Co-authored-by: Red <red@codrabbits.com>

--- CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
 
 namespace CodeRabbits.KaoList.Web.Areas.Identity.Pages.Account
 {
--- CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,6 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CodeRabbits.KaoList.Web.Areas.Identity.Pages.Account
 {
