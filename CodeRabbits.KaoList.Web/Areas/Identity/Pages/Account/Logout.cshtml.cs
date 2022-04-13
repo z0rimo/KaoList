ad3d819df3335d0bed26b5569933557b26b6dbbe
Red <red@coderabbits.com>
2022-04-14 02:23:56 +0900

    Fix login page (#54)
    * Remove nullable disable
    
    * Modify logo path, footer path
    
    * Modify register path
    
    * Modify logo path to index
    
    * Replace with correct login page

--- CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ CodeRabbits.KaoList.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,7 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
