1c5a5da2cdde42accb84d3abbd5dd4506ca6bf4d
Black <black@coderabbits.com>
2022-05-17 02:20:47 +0900

    Removed Bookmarks

--- CodeRabbits.KaoList.Data/KaoListUser.cs
+++ CodeRabbits.KaoList.Data/KaoListUser.cs
@@ -5,6 +5,5 @@ namespace CodeRabbits.KaoList.Data
     public class KaoListUser : IdentityUser
     {
         public string? NickName { get; set; }
-        public virtual IEnumerable<Bookmark>? Bookmarks { get; set; }
     }
 }
