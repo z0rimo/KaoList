1dc9d6596996f7bbaf03deff97f616d716e50f21
Red <red@coderabbits.com>
2022-06-07 00:39:42 +0900

    Create naver login (#86)
    * Create naverdefaults
    
    * Create naveroptions
    
    * Create naverhandler
    
    * Create naverchallengeproperties
    
    * Rename authenticationscheme to authenticationscheme
    
    * Create naverextensions
    
    * Modify handler, options and Add csproj, sln
    
    * Added the mit license in the summary and added a description of the function.
    
    * Modify mit license comment text
    
    * Add new constructor to scope
    
    * Edit comments

---
+++ CodeRabbits.Naver/NaverExtensions.cs
@@ -0,0 +1,67 @@
+﻿// (c) 2022 CodeRabbits.
+// This code is licensed under MIT license (see LICENSE.txt for details).
+
+using CodeRabbits.Naver;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods to configure Naver OAuth authentication.
+/// </summary>
+public static class NaverExtensions
+{
+    /// <summary>
+    /// Adds Naver OAuth-based authentication to <see cref="AuthenticationBuilder"/> using the default scheme.
+    /// The default scheme is specified by <see cref="NaverDefaults.AuthenticationScheme"/>.
+    /// <para>
+    /// Naver authentication allows application users to sign in with their Naver account.
+    /// </para>
+    /// </summary>
+    /// <param name="builder">The <see cref="AuthenticationBuilder"/>.</param>
+    /// <returns>A reference to <paramref name="builder"/> after the operation has completed.</returns>
+    public static AuthenticationBuilder AddNaver(this AuthenticationBuilder builder)
+        => builder.AddNaver(NaverDefaults.AuthenticationScheme, _ => { });
+
+    /// <summary>
+    /// Adds Naver OAuth-based authentication to <see cref="AuthenticationBuilder"/> using the default scheme.
+    /// The default scheme is specified by <see cref="NaverDefaults.AuthenticationScheme"/>.
+    /// <para>
+    /// Naver authentication allows application users to sign in with their Naver account.
+    /// </para>
+    /// </summary>
+    /// <param name="builder">The <see cref="AuthenticationBuilder"/>.</param>
+    /// <param name="configureOptions">A delegate to configure <see cref="NaverOptions"/>.</param>
+    /// <returns>A reference to <paramref name="builder"/> after the operation has completed.</returns>
+    public static AuthenticationBuilder AddNaver(this AuthenticationBuilder builder, Action<NaverOptions> configureOptions)
+        => builder.AddNaver(NaverDefaults.AuthenticationScheme, configureOptions);
+
+    /// <summary>
+    /// Adds Naver OAuth-based authentication to <see cref="AuthenticationBuilder"/> using the default scheme.
+    /// The default scheme is specified by <see cref="NaverDefaults.AuthenticationScheme"/>.
+    /// <para>
+    /// Naver authentication allows application users to sign in with their Naver account.
+    /// </para>
+    /// </summary>
+    /// <param name="builder">The <see cref="AuthenticationBuilder"/>.</param>
+    /// <param name="authenticationScheme">The authentication scheme.</param>
+    /// <param name="configureOptions">A delegate to configure <see cref="NaverOptions"/>.</param>
+    /// <returns>A reference to <paramref name="builder"/> after the operation has completed.</returns>
+    public static AuthenticationBuilder AddNaver(this AuthenticationBuilder builder, string authenticationScheme, Action<NaverOptions> configureOptions)
+        => builder.AddNaver(authenticationScheme, NaverDefaults.DisplayName, configureOptions);
+
+    /// <summary>
+    /// Adds Naver OAuth-based authentication to <see cref="AuthenticationBuilder"/> using the default scheme.
+    /// The default scheme is specified by <see cref="NaverDefaults.AuthenticationScheme"/>.
+    /// <para>
+    /// Naver authentication allows application users to sign in with their Naver account.
+    /// </para>
+    /// </summary>
+    /// <param name="builder">The <see cref="AuthenticationBuilder"/>.</param>
+    /// <param name="authenticationScheme">The authentication scheme.</param>
+    /// <param name="displayName">A display name for the authentication handler.</param>
+    /// <param name="configureOptions">A delegate to configure <see cref="NaverOptions"/>.</param>
+    /// <returns>A reference to <paramref name="builder"/> after the operation has completed.</returns>
+    public static AuthenticationBuilder AddNaver(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<NaverOptions> configureOptions)
+        => builder.AddOAuth<NaverOptions, NaverHandler>(authenticationScheme, displayName, configureOptions);
+}
---
+++ CodeRabbits.Naver/NaverHandler.cs
@@ -0,0 +1,104 @@
+﻿// (c) 2022 CodeRabbits.
+// This code is licensed under MIT license (see LICENSE.txt for details).
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace CodeRabbits.Naver;
+
+/// <summary>
+/// Authentication handler for Naver's OAuth based authentication.
+/// </summary>
+public class NaverHandler : OAuthHandler<NaverOptions>
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="NaverHandler"/>.
+    /// </summary>
+    /// <inheritdoc />
+    public NaverHandler(IOptionsMonitor<NaverOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    { }
+
+    /// <inheritdoc />
+    protected override async Task<AuthenticationTicket> CreateTicketAsync(
+        ClaimsIdentity identity,
+        AuthenticationProperties properties,
+        OAuthTokenResponse tokens)
+    {
+        // Get the Naver user
+        var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+
+        var response = await Backchannel.SendAsync(request, Context.RequestAborted);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException($"An error occurred when retrieving Naver user information ({response.StatusCode}). Please check if the authentication information is correct.");
+        }
+
+        using var payload = JsonDocument.Parse(await response.Content.ReadAsStringAsync(Context.RequestAborted));
+        var context = new OAuthCreatingTicketContext(new ClaimsPrincipal(identity), properties, Context, Scheme, Options, Backchannel, tokens, payload.RootElement);
+        context.RunClaimActions();
+        await Events.CreatingTicket(context);
+        return new AuthenticationTicket(context.Principal!, context.Properties, Scheme.Name);
+    }
+
+    /// <inheritdoc />
+    protected override string BuildChallengeUrl(AuthenticationProperties properties, string redirectUri)
+    {
+        var queryStrings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "response_type", "code" },
+            { "client_id", Options.ClientId },
+            { "redirect_uri", redirectUri }
+        };
+
+        AddQueryString(queryStrings, properties, NaverChallengeProperties.ScopeParameterkey);
+
+        var state = Options.StateDataFormat.Protect(properties);
+        queryStrings.Add("state", state);
+
+        var authorizationEndpoint = QueryHelpers.AddQueryString(Options.AuthorizationEndpoint, queryStrings!);
+        return authorizationEndpoint;
+    }
+
+    private static void AddQueryString<T>(
+        IDictionary<string, string> queryStrings,
+        AuthenticationProperties properties,
+        string name,
+        Func<T, string?> formatter,
+        T defaultValue)
+    {
+        string? value;
+        var parameterValue = properties.GetParameter<T>(name);
+        if (parameterValue != null)
+        {
+            value = formatter(parameterValue);
+        }
+        else if (!properties.Items.TryGetValue(name, out value))
+        {
+            value = formatter(defaultValue);
+        }
+
+        // Remove the parameter from AuthenticationProperties so it won't be serialized into the state
+        properties.Items.Remove(name);
+
+        if (value != null)
+        {
+            queryStrings[name] = value;
+        }
+    }
+
+    private static void AddQueryString(
+        IDictionary<string, string> queryStrings,
+        AuthenticationProperties properties,
+        string name,
+        string? defaultValue = null)
+        => AddQueryString(queryStrings, properties, name, x => x, defaultValue);
+}
