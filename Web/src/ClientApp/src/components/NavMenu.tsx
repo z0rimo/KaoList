68f0998652d3dad088e4226fa35015e4ba881356
Black <black@coderabbits.com>
2022-06-09 14:58:49 +0900

    Reset React (#99)
    * Rename ClientApp to client-app
    
    * Reset react

--- Web/src/ClientApp/src/components/NavMenu.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-function NavMenu(props: React.HTMLAttributes<HTMLDivElement>) {
-    return (
-        <div {...props} />
-    )
-}
-
-export default React.memo(NavMenu);
