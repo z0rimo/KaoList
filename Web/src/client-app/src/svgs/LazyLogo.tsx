c5f879a71ae7b39e9f274fd7e0443f6048430587
Red <red@coderabbits.com>
2022-09-06 09:33:56 +0900

    Add logo svg (#124)

---
+++ Web/src/client-app/src/svgs/LazyLogo.tsx
@@ -0,0 +1,18 @@
+import React, { Suspense } from "react"
+
+const Logo = React.lazy(() => import('./Logo'));
+
+function LazyLogo(props: React.HTMLAttributes<SVGSVGElement>) {
+    return (
+        <Suspense fallback={(<svg width="300"
+            height="60"
+            viewBox="0 0 300 60"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        />)}>
+            <Logo {...props} />
+        </Suspense>
+    )
+}
+
+export default React.memo(LazyLogo);
