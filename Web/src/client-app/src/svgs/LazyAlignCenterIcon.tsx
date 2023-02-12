7ce4d932c90b2c9dd6c7205acc8521641a273cc5
Violet <violet@coderabbits.com>
2023-02-12 20:53:00 +0900

    Rename icon align center (#335)
    * Delete textaligncenter
    
    * Add AlignCenterIcon

--- Web/src/client-app/src/svgs/LazyAlignCenterIcon.tsx
+++ Web/src/client-app/src/svgs/LazyAlignCenterIcon.tsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from "react"
 
-const TextAlignCenterIcon = React.lazy(() => import('./TextAlignCenterIcon'));
+const AlignCenterIcon = React.lazy(() => import('./AlignCenterIcon'));
 
-function LazyTextAlignCenterIcon(props: React.SVGAttributes<SVGSVGElement>) {
+function LazyAlignCenterIcon(props: React.SVGAttributes<SVGSVGElement>) {
     return (
         <Suspense fallback={(<svg width="15"
             height="15"
@@ -10,9 +10,9 @@ function LazyTextAlignCenterIcon(props: React.SVGAttributes<SVGSVGElement>) {
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         />)}>
-            <TextAlignCenterIcon {...props} />
+            <AlignCenterIcon {...props} />
         </Suspense>
     )
 }
 
-export default React.memo(LazyTextAlignCenterIcon);
+export default React.memo(LazyAlignCenterIcon);
