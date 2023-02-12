584db936a6e384dde74249a7d36c229fa5e428ac
Violet <violet@coderabbits.com>
2023-02-12 17:33:29 +0900

    Rename 'FileUpload' to 'Link' (#346)

--- Web/src/client-app/src/svgs/FileUploadIcon/FileUploadIcon.tsx
+++
@@ -1,1 +0,0 @@
-export { ReactComponent as default } from './fileUploadIcon.svg';
--- Web/src/client-app/src/svgs/LazyLinkIcon.tsx
+++ Web/src/client-app/src/svgs/LazyLinkIcon.tsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from "react"
 
-const FileUploadIcon = React.lazy(() => import('./FileUploadIcon'));
+const LinkIcon = React.lazy(() => import('./LinkIcon'));
 
-function LazyFileUploadIcon(props: React.SVGAttributes<SVGSVGElement>) {
+function LazyLinkIcon(props: React.SVGAttributes<SVGSVGElement>) {
     return (
         <Suspense fallback={(<svg width="17"
             height="15"
@@ -10,9 +10,9 @@ function LazyFileUploadIcon(props: React.SVGAttributes<SVGSVGElement>) {
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         />)}>
-            <FileUploadIcon {...props} />
+            <LinkIcon {...props} />
         </Suspense>
     )
 }
 
-export default React.memo(LazyFileUploadIcon);
+export default React.memo(LazyLinkIcon);
