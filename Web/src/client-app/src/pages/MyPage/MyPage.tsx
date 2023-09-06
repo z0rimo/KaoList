2448f4b3311bd8a2a1ecbaa9de64223eee4d4640
red <red@coderabbits.com>
2023-09-07 00:17:48 +0900

    Code cleanup

--- Web/src/client-app/src/pages/MyPage/MyPage.tsx
+++ Web/src/client-app/src/pages/MyPage/MyPage.tsx
@@ -3,6 +3,7 @@ import MainLayout from '../../layouts/MainLayout/MainLayout';
 import MainSection from '../../components/MainSection';
 import MyPageProfileList from '../../components/MyPageProfileList/MyPageProfileList';
 import MyPageActivityList from '../../components/MyPageActivityList/MyPageActivityList';
+import './MyPage.scss';
 
 function MyPage() {
     return (
