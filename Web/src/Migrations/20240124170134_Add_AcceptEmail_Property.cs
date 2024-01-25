2418336c39a62b42101f098c66cf2b8386eab580
red <pfcgm24@naver.com>
2024-01-25 23:48:44 +0900

    Snapshot

---
+++ Web/src/Migrations/20240124170134_Add_AcceptEmail_Property.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeRabbits.KaoList.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_AcceptEmail_Property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptEmail",
+                table: "KaoListUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptEmail",
+                table: "KaoListUsers");
+        }
+    }
+}
