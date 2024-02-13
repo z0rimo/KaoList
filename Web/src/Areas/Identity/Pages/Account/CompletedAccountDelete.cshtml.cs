a79a5f94339ebd2a59d2f00eeebec5ec8b54aa59
red <pfcgm24@naver.com>
2024-02-14 00:36:26 +0900

    Create accoumt delete complete

---
+++ Web/src/Areas/Identity/Pages/Account/CompletedAccountDelete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeRabbits.KaoList.Web.Areas.Identity.Pages.Account
+{
+    public class CompletedAccountDeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
