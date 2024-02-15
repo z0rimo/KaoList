de51b3cc28634c2716d73c5488e54c9ea3e8ef82
red <pfcgm24@naver.com>
2024-02-16 01:20:14 +0900

    Remove summary

--- Web/src/scripts/delete-account.ts
+++ Web/src/scripts/delete-account.ts
@@ -15,17 +15,12 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const updateButtonState = () => {
-    // const isAnyCheckboxChecked = Array.prototype.slice.call(checkboxes).some((checkbox: HTMLInputElement) => checkbox.checked);
-    // const isAgreeChecked = agreeCheckbox.checked;
-    // confirmButton.disabled = !(isAnyCheckboxChecked && isAgreeChecked);
-
-    // 이유(reason) 중 하나라도 체크되어 있는지 확인
-    var isAnyReasonChecked = Array.prototype.slice.call(reasonCheckboxes).some(function (checkbox: HTMLInputElement) {
+    const isAnyReasonChecked = Array.prototype.slice.call(reasonCheckboxes).some(function (checkbox: HTMLInputElement) {
       return checkbox.checked;
     });
-    // 동의 체크박스가 체크되어 있는지 확인
-    var isAgreeChecked = agreeCheckbox.checked;
-    // 이유와 동의 체크박스 모두 체크되어 있어야 버튼 활성화
+
+    const isAgreeChecked = agreeCheckbox.checked;
+
     confirmButton.disabled = !(isAnyReasonChecked && isAgreeChecked);
   };
 
