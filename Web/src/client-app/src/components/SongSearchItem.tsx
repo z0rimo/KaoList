336fd5c0ad0c109b2814808b7d79d43bb57e3b3f
red <pfcgm24@naver.com>
2024-03-26 18:53:45 +0900

    Activate click events

--- Web/src/client-app/src/components/SongSearchItem.tsx
+++ Web/src/client-app/src/components/SongSearchItem.tsx
@@ -5,6 +5,9 @@ import { ISongSearchListItem } from "../SongSearchList";
 import { useTranslation } from "react-i18next";
 import StringHelper from "../StringHelper";
 import HighlightMatch from "./HighlightMatch";
+import { useLocation, useNavigate } from "react-router-dom";
+import { queryParameterNames } from "./identity";
+import { useIdentityContext } from "../contexts/IdentityContext";
 
 interface ISongSearchItemProps {
     item: ISongSearchListItem;
@@ -12,8 +15,13 @@ interface ISongSearchItemProps {
 }
 
 function SongSearchItem({ item, q }: ISongSearchItemProps) {
-    const [like, setLike] = React.useState<boolean>(false);
     const { t } = useTranslation('Chart')
+    const identityContext = useIdentityContext();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [like, setLike] = React.useState<boolean>(false);
+    const isLoggedIn = identityContext.user ? true : false;
+
     let tjNo = "-";
     let kumyoungNo = "-";
 
@@ -23,12 +31,22 @@ function SongSearchItem({ item, q }: ISongSearchItemProps) {
         kumyoungNo = item.karaoke.no ?? "-";
     }
 
-    const navgiateToDetailClick = () => {
-        window.location.href = `/songs/detail?id=${item.id}`;
-    }
+    const navgiateToDetailClick = (e: React.MouseEvent<HTMLTableRowElement>) => {
+        navigate(`/songs/detail?id=${item.id}`);
+    };
+
+    const handleLikeClick = (e: React.MouseEvent<HTMLTableCellElement>) => {
+        e.stopPropagation();
+        if (!isLoggedIn) {
+            alert("좋아요 기능은 로그인을 하셔야합니다.");
+            navigate(`${window.authPaths.Login}?${queryParameterNames.ReturnUrl}=${encodeURIComponent(window.location.origin + location.pathname + `?q=${q}`)}`);
+            return;
+        }
+        setLike(!like);
+    };
 
     return (
-        <tr className="tr-group fs-4">
+        <tr className="tr-group fs-4" onClick={navgiateToDetailClick} style={{ cursor: 'pointer' }}>
             <td className="center-layout">
                 <img alt={StringHelper.format(t('Thumbnail of {0}'), item.title)}
                     src="https://i.ytimg.com/vi/XOxI7bEHQgc/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5kqwJDiTRyMg0D5mIsZ0ZyTcvRg" />
@@ -48,7 +66,7 @@ function SongSearchItem({ item, q }: ISongSearchItemProps) {
             </td>
             <td>{tjNo}</td>
             <td>{kumyoungNo}</td>
-            <td>
+            <td onClick={handleLikeClick}>
                 {like ? <LazyStarSolidIcon fill="#6BB9A4" /> : <LazyStarIcon fill="#5F6368" />}
             </td>
         </tr>
