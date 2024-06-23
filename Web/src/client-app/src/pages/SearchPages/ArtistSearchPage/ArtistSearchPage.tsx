58d4f6a57f794dcdcae2574ac137e6f36c1c2b85
red <pfcgm24@naver.com>
2024-06-23 22:09:50 +0900

    Add arist search page

---
+++ Web/src/client-app/src/pages/SearchPages/ArtistSearchPage/ArtistSearchPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './ArtistSearchPage.scss';
+
+function ArtistSearchPage(props: React.HtmlHTMLAttributes<HTMLDivElement>) {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(ArtistSearchPage);
