e517c2df08e0b7929b21c1b69e29784b56556011
red <pfcgm24@naver.com>
2024-07-03 22:52:27 +0900

    Create song search page basic

---
+++ Web/src/client-app/src/pages/SearchPages/SongSearchPage/SongSearchPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './SongSearchPage.scss';
+
+function SongSearchPage(props: React.HtmlHTMLAttributes<HTMLDivElement>) {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(SongSearchPage);
